345d2d4f65dec53c6083f59ca6ae380374ac61d7
huba.gaspar <gaspar.huba91@gmail.com>
2021-05-17 12:48:01 +0200

    Layout and image sizing fixes

--- components/sections/MainSlideshow/MainSlideshow.tsx
+++ components/sections/MainSlideshow/MainSlideshow.tsx
@@ -7,18 +7,18 @@ function MainSlideshow(): ReactElement {
   return (
     <div className={utilsStyles.section}>
       <InfiniteSlideshow>
-        <Image src="/images/slide00.jpg" width={1536} height={2048} />
-        <Image src="/images/slide01.jpg" width={1536} height={2048} />
-        <Image src="/images/slide02.jpg" width={1536} height={2048} />
-        <Image src="/images/slide03.jpg" width={1536} height={2048} />
-        <Image src="/images/slide04.jpg" width={1536} height={2048} />
-        <Image src="/images/slide05.jpg" width={1536} height={2048} />
-        <Image src="/images/slide06.jpg" width={1536} height={2048} />
-        <Image src="/images/slide07.jpg" width={1536} height={2048} />
-        <Image src="/images/slide08.jpg" width={1536} height={2048} />
-        <Image src="/images/slide09.jpg" width={1536} height={2048} />
-        <Image src="/images/slide10.jpg" width={1536} height={2048} />
-        <Image src="/images/slide11.jpg" width={1536} height={2048} />
+        <Image src="/images/slide00.jpg" width={1536} height={2048} layout="responsive" />
+        <Image src="/images/slide01.jpg" width={1536} height={2048} layout="responsive" />
+        <Image src="/images/slide02.jpg" width={1536} height={2048} layout="responsive" />
+        <Image src="/images/slide03.jpg" width={1536} height={2048} layout="responsive" />
+        <Image src="/images/slide04.jpg" width={1536} height={2048} layout="responsive" />
+        <Image src="/images/slide05.jpg" width={1536} height={2048} layout="responsive" />
+        <Image src="/images/slide06.jpg" width={1536} height={2048} layout="responsive" />
+        <Image src="/images/slide07.jpg" width={1536} height={2048} layout="responsive" />
+        <Image src="/images/slide08.jpg" width={1536} height={2048} layout="responsive" />
+        <Image src="/images/slide09.jpg" width={1536} height={2048} layout="responsive" />
+        <Image src="/images/slide10.jpg" width={1536} height={2048} layout="responsive" />
+        <Image src="/images/slide11.jpg" width={1536} height={2048} layout="responsive" />
       </InfiniteSlideshow>
     </div>
   );
